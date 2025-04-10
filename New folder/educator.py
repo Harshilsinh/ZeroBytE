2edd499d58d49e74c6dff3792813216ab853480c
Harshil Kanuji Rajput <harshilsinh1516@gmail.com>
2025-04-10 11:45:00 +0530

    Delete New folder directory

--- New folder/educator.py
+++
@@ -1,8 +0,0 @@
-class FoodWasteEducator:
-    @staticmethod
-    def fetch_tips():
-        return [
-            "Store food properly to extend shelf life.",
-            "Use leftovers creatively instead of discarding them.",
-            "Plan meals ahead to reduce unnecessary purchases."
-        ]
